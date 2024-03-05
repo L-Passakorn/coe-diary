1ebd061fb441d3f97660523f0d710d81c32f629c
Passakorn Limseng <6510110356@email.psu.ac.th>
2024-03-05 19:36:23 +0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import Feed from "@components/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Let's post
+        <br className='max-md:hidden' />
+        <span className="orange_gradient text-center"> How you feel rn in CoE?</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni tempore
+        provident labore. Officia, quasi enim, veritatis dolores quae ipsum in
+        beatae nemo atque quod, tenetur eius doloribus? Ducimus, tenetur iste.
+      </p>
+
+      {/* Feed */}
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
